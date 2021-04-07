38d765865dac533b1feaccd8cb5af30597711df6
asoko3 <asoko3@softserveinc.com>
2021-04-07 21:27:27 +0300

    create crud service and resolvers to get mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PizzaOrderHomeComponent } from './feature/components';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, PizzaOrderHomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/feature/services/pizza-order-crud/pizza-order-crud.service.ts
+++ src/app/feature/services/pizza-order-crud/pizza-order-crud.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PizzaItemInterface, PizzeriaItemInterface } from '../../interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PizzaOrderCrudService {
+  private readonly url = 'assets/mock-data';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getPizzaList(): Observable<PizzaItemInterface[]> {
+    return this.httpClient.get<PizzaItemInterface[]>(
+      `${this.url}/pizzas-list.json`
+    );
+  }
+
+  getPizzeriasList(): Observable<PizzeriaItemInterface[]> {
+    return this.httpClient.get<PizzeriaItemInterface[]>(
+      `${this.url}/pizzerias-list.json`
+    );
+  }
 }
